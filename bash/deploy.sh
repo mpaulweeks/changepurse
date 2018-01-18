131f5c3114df2e4be684de14aca63703b431ec85
mpaulweeks <mpaulweeks@gmail.com>
2018-01-18 00:54:19 -0500

    fix static deploy

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -2,15 +2,15 @@ cd angular-app
 npm run build
 
 mkdir ../docs
-mv ../docs/static/ _static
+mv ../docs/static/ _static/
 rm -r ../docs
 
 mkdir ../docs
 cp -a dist/ ../docs
-mv _static ../docs/
+mv _static/ ../docs/static/
 cp README_BUILD.md ../docs/README.md
 
 cd ..
-git add .
-git commit -m 'deploy'
-git push
+# git add .
+# git commit -m 'deploy'
+# git push
