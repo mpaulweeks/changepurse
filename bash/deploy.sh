cbe9e8cbacb7d7fcc6cf57cfab39985151714b70
mpaulweeks <mpaulweeks@gmail.com>
2018-01-18 00:26:19 -0500

    amend deploy script to preserve static

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -2,10 +2,12 @@ cd angular-app
 npm run build
 
 mkdir ../docs
+mv ../docs/static/ _static
 rm -r ../docs
 
 mkdir ../docs
 cp -a dist/ ../docs
+mv _static ../docs/
 cp README_BUILD.md ../docs/README.md
 
 cd ..
