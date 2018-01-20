9300cb3d733a8b8577ba39005ea3fee59c0abc23
mpaulweeks <mpaulweeks@gmail.com>
2018-01-19 20:49:23 -0500

    add angular locally, update makefile, add price autofill

--- docs/angularjs/todo.js
+++ docs/angularjs/todo.js
@@ -1,35 +1,46 @@
 
-angular.module('todoApp', [])
-  .controller('TodoListController', function($scope) {
-    var todoList = this;
-    todoList.todos = [
+angular.module('changePurseApp', [])
+  .controller('ChangePurseController', function($scope) {
+    const self = this;
+    self.todos = [
       {text:'learn AngularJS', done:true},
       {text:'build an AngularJS app', done:false}];
 
-    todoList.addTodo = function() {
-      todoList.todos.push({text:todoList.todoText, done:false});
-      todoList.todoText = '';
+    self.calcPriceSum = function() {
+      const quantity = parseFloat(self.newQuantity, 10);
+      const pricePer = parseFloat(self.newPricePer, 10);
+      if (!isNaN(quantity) && !isNaN(pricePer)){
+        self.newPriceSum = pricePer * quantity;
+      }
+    };
+    self.calcPricePer = function() {
+      const quantity = parseFloat(self.newQuantity, 10);
+      const priceSum = parseFloat(self.newPriceSum, 10);
+      if (!isNaN(quantity) && !isNaN(priceSum)){
+        self.newPricePer = priceSum / quantity;
+      }
+    };
+    self.calcPrice = function(){
+      self.calcPricePer();
+      self.calcPriceSum();
+    }
+
+    self.addTodo = function() {
+      self.todos.push({text:self.todoText, done:false});
+      self.todoText = '';
     };
 
-    todoList.remaining = function() {
-      var count = 0;
-      angular.forEach(todoList.todos, function(todo) {
+    self.remaining = function() {
+      let count = 0;
+      angular.forEach(self.todos, function(todo) {
         count += todo.done ? 0 : 1;
       });
       return count;
     };
 
-    todoList.archive = function() {
-      var oldTodos = todoList.todos;
-      todoList.todos = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) todoList.todos.push(todo);
-      });
-    };
-
-    const NAMES_URL = 'https://raw.githubusercontent.com/mpaulweeks/changepurse/master/docs/static/ticker_names.json';
+    const NAMES_URL = 'https://raw.githubusercontent.com/mpaulweeks/changepurse/master/docs/ticker_names.json';
     fetch(NAMES_URL).then(r => r.json()).then(lookup => {
-      todoList.todos.push({
+      self.todos.push({
         text: lookup['ETH'],
         done: false,
       });
--- makefile
+++ makefile
@@ -1,5 +1,2 @@
 start:
-	cd angular-app && npm run open
-
-deploy:
-	./bash/deploy.sh
+	cd docs && python -m SimpleHTTPServer
