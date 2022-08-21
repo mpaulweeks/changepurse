87cf7f827d54185143087dbb271b26968f15f4a5
mpaulweeks <mpaulweeks@gmail.com>
2022-08-21 13:51:46 -0400

    upload to s3

--- node/src/scrape.js
+++ node/src/scrape.js
@@ -2,14 +2,18 @@ import 'dotenv/config'
 import fs from 'fs';
 import fetch from 'node-fetch';
 import stringify from 'json-stable-stringify';
+import AWS from 'aws-sdk';
 
-
-const apiKey = process.env.COINMARKETCAP_API_KEY;
+const AwsBucket = process.env.AWS_BUCKET;
+const AwsConfig = {
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+};
 const RequestInfo = {
   Url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
   Headers: {
     'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': apiKey,
+    'X-CMC_PRO_API_KEY': process.env.COINMARKETCAP_API_KEY,
   },
   QueryParams: new URLSearchParams({
     'start': 1,
@@ -37,13 +41,35 @@ function reduceCoins(coins) {
   }, {});
 }
 
-async function writeToFile(name, data) {
+function fileToString(name, fileData) {
+  return [{
+    key: `${name}.min.json`,
+    value: stringify(fileData),
+  },  {
+    key: `${name}.json`,
+    value: stringify(fileData, { space: 2, }),
+  }];
+}
+
+async function writeToFile(files) {
   const folder = 'tmp';
   if (!fs.existsSync(folder)){
     fs.mkdirSync(folder);
   }
-  await fs.promises.writeFile(`${folder}/${name}.min.json`, stringify(data));
-  await fs.promises.writeFile(`${folder}/${name}.json`, stringify(data, { space: 2, }));
+  for (const file of files) {
+    await fs.promises.writeFile(`${folder}/${file.key}`, file.value);
+  }
+}
+
+async function uploadToS3(files) {
+  const s3 = new AWS.S3(AwsConfig);
+  for (const file of files) {
+    await s3.upload({
+      Bucket: AwsBucket,
+      Key: file.key,
+      Body: file.value,
+    }).promise();
+  }
 }
 
 (async () => {
@@ -51,9 +77,12 @@ async function writeToFile(name, data) {
   // console.log(resp);
   const coins = reduceCoins(resp.data);
   console.log('coins founds:', Object.keys(coins).length);
-  await writeToFile('price', {
+
+  const files = fileToString('price', {
     updated: new Date().toISOString(),
     coins,
   });
+  await writeToFile(files);
+  await uploadToS3(files);
   console.log('done');
 })();
